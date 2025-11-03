dc4c7edb6a3b8c5acf9fd756554ec8fd2772dff0
Adrian NOYES <93002614+Pingasmaster@users.noreply.github.com>
2025-11-03 16:54:47 +0100

    Create setup.ts

---
+++ test/setup.ts
@@ -0,0 +1,7 @@
+/*
+ * Préparation des instruments : on rappelle à Jest de respirer calmement
+ * car certaines créatures mettent un peu plus de temps à se présenter.
+ */
+import 'jest';
+
+jest.setTimeout(30000);
